220564e546c0d62820dc2030cecec770cc80a0e3
haejin <luckyhaejin1126@gmail.com>
2024-02-13 10:34:32 +0900

    json server, API 호출, ProductCard

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src ="https://noona-hnm.netlify.app/one-shoulder-dress.jpeg" />
+      <div>Consicous choice</div>
+      <div>라인스톤 스트랩 원숄더 드레스</div>
+      <div></div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setProductList(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  // 카드 가운데, 한줄에 네개씩, 반응형 웹사이트
   return (
     <div>
       <h1>상품 전체 페이지</h1>
+      <ProductCard />
     </div>
   )
 }
